c2a645e860ed6fea62ffc62bff76a668975d5cbb
DaniIliev <114102749+DaniIliev@users.noreply.github.com>
2023-06-23 15:40:23 +0300

    add createPage

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,11 @@ const router = require('express').Router()
 
 const homeController = require('./controllers/homeController')
 const userController = require('./controllers/userController')
+const pageController = require('./controllers/pageController')
+
 
 router.use('/user', userController)
 router.get('/', homeController.getHomePage)
-
+router.use(pageController)
 
 module.exports = router
